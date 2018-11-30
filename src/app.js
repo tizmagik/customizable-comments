c246aadb1db9228e2e439777b315be91bbc73fa9
Jeremy Gayed <jeremy.gayed@nytimes.com>
2018-11-30 12:25:12 -0500

    stuff

--- src/app.js
+++ src/app.js
@@ -6,17 +6,12 @@ const PR_OPENED = "pull_request.opened";
 module.exports = app => {
   app.log("customizable-comments probot app loaded");
 
-  // app.on("issues.opened", async context => {
-  //   const issueComment = context.issue({
-  //     body: "Thanks for opening this issue!"
-  //   });
-  //   return context.github.issues.createComment(issueComment);
-  // });
-
   app.on(PR_OPENED, async context => {
     const { github /*, log, payload*/ } = context;
 
     const body = await template(context, PR_OPENED);
+
+    if (!body) return;
 
     const params = context.issue({ body });
 
