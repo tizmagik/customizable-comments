894ee2593197165fd9ae6babd27839240a84ff65
Jeremy Gayed <jeremy.gayed@nytimes.com>
2018-12-03 12:47:20 -0500

    Tests

---
+++ src/__tests__/app.test.js
@@ -0,0 +1,30 @@
+const { Application } = require("probot");
+const autoreplyPreviewHelpersBot = require("../");
+const payload = require("./fixtures/pull_request.opened.json");
+
+jest.mock("probot-config");
+
+describe("probot-cc", () => {
+  let app;
+  let github;
+
+  beforeEach(() => {
+    app = new Application();
+    app.load(autoreplyPreviewHelpersBot);
+    github = {
+      issues: {
+        createComment: jest.fn().mockReturnValue(Promise.resolve())
+      }
+    };
+    app.auth = () => Promise.resolve(github);
+  });
+
+  describe("on pull_request.opened", () => {
+    it("creates comment with preview sandbox helpers", async () => {
+      await app.receive(payload);
+
+      expect(github.issues.createComment).toHaveBeenCalled();
+      expect(github.issues.createComment.mock.calls[0][0]).toMatchSnapshot();
+    });
+  });
+});
--- src/config.js
+++ src/config.js
@@ -2,8 +2,28 @@ const probotConfig = require("probot-config");
 
 const CONFIG_NAME = "customizable-comments.yml";
 
+/*
+
+pull_request:
+  opened:
+    template: |
+      Here's a helpful URL based on the branch name: https://$BRANCH-$BRANCH_SUFFIX.something.example.com
+      Second line goes here.
+      BRANCH is: $BRANCH
+      BRANCH_SUFFIX is: $BRANCH_SUFFIX
+      Done.
+
+
+*/
+
 const getConfig = async context => {
-  const defaultConfig = {};
+  const defaultConfig = {
+    pull_request: {
+      opened: {
+        template: `_You have succesfully installed probot-cc, now please create a \`.github/${CONFIG_NAME}\` file in your repository to customize this message._`
+      }
+    }
+  };
 
   const config = (await probotConfig(context, CONFIG_NAME)) || defaultConfig;
 
