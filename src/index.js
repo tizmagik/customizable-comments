3b95f6c6ef821ea91201e890fb3a532aa01612d9
tizmagik <tizmagik@users.noreply.github.com>
2021-04-22 15:48:43 -0400

    disable GCF for now

--- src/index.js
+++ src/index.js
@@ -2,4 +2,6 @@ const app = require('./app');
 // const handler = require('./handler');
 
 module.exports = app;
+// TODO(tizmagik): Apr 22, 2021: Disabling for now as it looks like probot core will support serverless out of the box,
+// see: https://github.com/probot/serverless-gcf/issues/17
 // module.exports.probot = handler; // GCF handler
